b8f98ed24eb8eceab54f92f392f86402e6202b32
sarah-campos <sarahstephanycampos@gmail.com>
2024-07-18 19:06:50 -0300

    scroll suave + settings

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,12 @@ import initTranslations from "./modules/translations.js";
 import initScroll from "./modules/scroll.js";
 import initPlayerMusic from "./modules/playerMusic.js";
 import initSlider from "./modules/imageSlider.js";
+import initScrollSuave from "./modules/scrollSuave.js";
 
 
 initTranslations();
 initScroll();
 initPlayerMusic();
 initSlider();
+initScrollSuave();
 
